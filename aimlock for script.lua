dc4e1a55e614159b218d3ae007db7d6e4cc45733
toidqwe <108140546+toidqwe@users.noreply.github.com>
2022-10-01 20:24:57 +0900

    asdasds
    asdasd

---
+++ aimlock for script.lua
@@ -0,0 +1,30 @@
+getgenv().Aimbot
+getgenv().Aimbot.Settings.Enabled = true
+
+getgenv().Aimbot.Settings = {
+    SendNotifications = true,
+    SaveSettings = true, -- Re-execute upon changing
+    ReloadOnTeleport = true,
+    Enabled = true,
+    TeamCheck = false,
+    AliveCheck = true,
+    WallCheck = false, -- Laggy
+    Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
+    TriggerKey = "MouseButton2",
+    Toggle = false,
+    LockPart = "Head" -- Body part to lock on (Character part's name)
+}
+
+getgenv().Aimbot.FOVSettings = {
+    Enabled = true,
+    Visible = true,
+    Amount = 90,
+    Color = "255, 255, 255",
+    LockedColor = "255, 70, 70",
+    Transparency = 0.5,
+    Sides = 60,
+    Thickness = 1,
+    Filled = false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V2/main/Resources/Scripts/Delete%20Settings%20-%20Fix%20Script.lua"))()
